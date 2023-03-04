d37da426a0e39f54ff51425dab5f43154ed5bf79
Artemistic-I <93589100+Artemistic-I@users.noreply.github.com>
2023-03-04 14:32:52 +0000

    Added all models with initial schemas

---
+++ database/migrations/2023_02_27_101956_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->dateTime('created_at');
+            $table->integer('likes_count');
+            $table->integer('dislikes_count');
+            $table->integer('views_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
